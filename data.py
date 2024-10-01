b4ae2401ceaef87680596d20a3a010e394d3ade1
InesZU <inesrahrah@gmail.com>
2024-10-01 13:23:36 +0200

    update data file

--- data.py
+++ data.py
@@ -39,12 +39,13 @@ class FlightData:
 
     def get_flights_by_date(self, day, month, year):
         """
-        Fetch all flights on a specific date.
+        Fetch all flights on a specific date, including airline names.
         """
         QUERY_FLIGHTS_BY_DATE = """
         SELECT flights.ID, flights.ORIGIN_AIRPORT, flights.DESTINATION_AIRPORT, 
-               flights.DEPARTURE_DELAY AS DELAY, flights.AIRLINE
+               flights.DEPARTURE_DELAY AS DELAY, airlines.AIRLINE
         FROM flights
+        JOIN airlines ON flights.AIRLINE = airlines.ID
         WHERE flights.DAY = :day
         AND flights.MONTH = :month
         AND flights.YEAR = :year
@@ -61,7 +62,7 @@ class FlightData:
                flights.DEPARTURE_DELAY AS DELAY, airlines.AIRLINE
         FROM flights
         JOIN airlines ON flights.AIRLINE = airlines.ID
-        WHERE airlines.AIRLINE LIKE :airline_name
+        WHERE airlines.AIRLINE LIKE :airline_name AND flights.DEPARTURE_DELAY > 20
         """
         params = {'airline_name': f'%{airline_name}%'}
         return self._execute_query(QUERY_DELAYED_FLIGHTS_BY_AIRLINE, params)
@@ -75,7 +76,7 @@ class FlightData:
                flights.DEPARTURE_DELAY AS DELAY, airlines.AIRLINE
         FROM flights
         JOIN airlines ON flights.AIRLINE = airlines.ID
-        WHERE flights.ORIGIN_AIRPORT = :IATA_code
+        WHERE flights.ORIGIN_AIRPORT = :IATA_code AND flights.DEPARTURE_DELAY > 20
         AND flights.DEPARTURE_DELAY > 0
         """
         params = {'IATA_code': airport_code}
